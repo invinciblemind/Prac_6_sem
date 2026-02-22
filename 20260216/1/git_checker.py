4d78d577d3bd1cc0041fbb6eeca47c8ddc07416f
Max Big <maxbig@MacBook-Air-Max.local>
2026-02-22 23:54:15 +0300

    added output of last commit in branch (second arg)

--- 20260216/1/git_checker.py
+++ 20260216/1/git_checker.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import zlib
 import sys
 import os
 
@@ -6,3 +8,9 @@ repo = args[1]
 if len(args) == 2:
     branches = os.listdir(repo + '/.git/refs/heads')
     print('\n'.join(branches))
+else:
+    branch = args[2]
+    f = open(repo + '/.git/refs/heads/' + branch)
+    text = f.readline()[:-1]
+    data = zlib.decompress(Path(repo + '/.git/objects/' + text[:2] + '/' + text[2:]).read_bytes())
+    print(data.decode())
