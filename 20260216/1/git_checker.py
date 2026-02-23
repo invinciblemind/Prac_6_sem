9a1e76320b0ac070a8536240f435fcefcb4339f2
Max Big <maxbig@MacBook-Air-Max.local>
2026-02-23 14:07:01 +0300

    added output of tree-objects of all history of commits

--- 20260216/1/git_checker.py
+++ 20260216/1/git_checker.py
@@ -16,6 +16,7 @@ else:
     text2 = data.split('tree')[1].split()[0]
     header, _, body = zlib.decompress(Path(repo + '/.git/objects/' + text2[:2] + '/' + text2[2:]).read_bytes()).partition(b'\x00')
     kind, size = header.split()
+    print('TREE for commit', text)
     while body:
         treeobj, _, body = body.partition(b'\x00')
         tmode, tname = treeobj.split()
@@ -23,3 +24,17 @@ else:
         header2, _, body2 = zlib.decompress(Path(repo + '/.git/objects/' + str(num.hex())[:2] + '/' + str(num.hex())[2:]).read_bytes()).partition(b'\x00')
         kind2, size2 = header2.split()
         print(f'{kind2.decode()} {num.hex()}   {tname.decode()}')
+    while 'parent' in data:
+        text = data.split('parent')[1].split()[0]
+        print('TREE for commit', text)
+        data = zlib.decompress(Path(repo + '/.git/objects/' + text[:2] + '/' + text[2:]).read_bytes()).decode()
+        text2 = data.split('tree')[1].split()[0]
+        header, _, body = zlib.decompress(Path(repo + '/.git/objects/' + text2[:2] + '/' + text2[2:]).read_bytes()).partition(b'\x00')
+        kind, size = header.split()
+        while body:
+            treeobj, _, body = body.partition(b'\x00')
+            tmode, tname = treeobj.split()
+            num, body = body[:20], body[20:]
+            header2, _, body2 = zlib.decompress(Path(repo + '/.git/objects/' + str(num.hex())[:2] + '/' + str(num.hex())[2:]).read_bytes()).partition(b'\x00')
+            kind2, size2 = header2.split()
+            print(f'{kind2.decode()} {num.hex()}   {tname.decode()}')
