4cfece0468bd098dc5a53f41af6cf09d23c32842
Max Big <maxbig@MacBook-Air-Max.local>
2026-03-08 17:40:52 +0300

    added custom jgsbat monster

--- 20260302/1/multi_user_dungeon.py
+++ 20260302/1/multi_user_dungeon.py
@@ -1,12 +1,33 @@
 import cowsay
 import sys
+from io import StringIO
 
 
 def encounter(x, y):
-    global monsters
-    print(cowsay.cowsay(monsters[(x, y)][1], cow=monsters[(x, y)][0]))
+    global monsters, jgsbat
+    if monsters[(x, y)][0] == 'jgsbat':
+        print(cowsay.cowsay(monsters[(x, y)][1], cowfile=jgsbat))
+    else:
+        print(cowsay.cowsay(monsters[(x, y)][1], cow=monsters[(x, y)][0]))
 
 
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\\/o\\/o\\/'.   `(
+      ) .' . \\====/ . '. (
+       )  / <<    >> \\  (
+        '-._/``  ``\\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+list_cows = cowsay.list_cows()
+list_cows.append('jgsbat')
 x, y = 0, 0
 monsters = {}
 cmd = sys.stdin.readline()
@@ -30,7 +51,7 @@ while cmd != '':
     elif len(cmd) == 5 and cmd[0] == 'addmon':
         cmd = [cmd[0], cmd[2], cmd[3], cmd[1], cmd[4]]
         if cmd[1].isdigit() and cmd[2].isdigit() and 0 <= int(cmd[1]) <= 9 and 0 <= int(cmd[2]) <= 9:
-            if cmd[3] not in cowsay.list_cows():
+            if cmd[3] not in list_cows:
                 print('Cannot add unknown monster')
             else:
                 print(f'Added monster {cmd[3]} to ({int(cmd[1])}, {int(cmd[2])}) saying {cmd[4]}')
