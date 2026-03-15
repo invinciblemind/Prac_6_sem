d5855e0c1a27f7ca74e05f7f464cd1b468333aca
Max Big <maxbig@MacBook-Air-Max.local>
2026-03-15 21:26:23 +0300

    copy of task1 from 02.03

---
+++ 20260312/1/multi_user_dungeon.py
@@ -0,0 +1,72 @@
+import cowsay
+import sys
+from io import StringIO
+import shlex
+
+
+def encounter(x, y):
+    global monsters, jgsbat
+    if monsters[(x, y)][0] == 'jgsbat':
+        print(cowsay.cowsay(monsters[(x, y)][2], cowfile=jgsbat))
+    else:
+        print(cowsay.cowsay(monsters[(x, y)][2], cow=monsters[(x, y)][0]))
+
+
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\\/o\\/o\\/'.   `(
+      ) .' . \\====/ . '. (
+       )  / <<    >> \\  (
+        '-._/``  ``\\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+list_cows = cowsay.list_cows()
+list_cows.append('jgsbat')
+x, y = 0, 0
+monsters = {}
+print("<<< Welcome to Python-MUD 0.1 >>>")
+cmd = sys.stdin.readline()
+while cmd != '':
+    cmd = shlex.split(cmd)
+    if len(cmd) == 1:
+        if cmd[0] == 'up':
+            y = (y + 9) % 10
+        elif cmd[0] == 'down':
+            y = (y + 1) % 10
+        elif cmd[0] == 'left':
+            x = (x + 9) % 10
+        elif cmd[0] == 'right':
+            x = (x + 1) % 10
+        else:
+            print('Invalid command')
+        if cmd[0] in ['up', 'down', 'left', 'right']:
+            print(f'Moved to ({x}, {y})')
+            if (x, y) in monsters:
+                encounter(x, y)
+    elif len(cmd) == 9 and cmd[0] == 'addmon':
+        name = cmd[1]
+        hello_str = cmd[cmd.index('hello') + 1]
+        hp = cmd[cmd.index('hp') + 1]
+        xx, yy = cmd[cmd.index('coords') + 1:cmd.index('coords') + 3]
+        if xx.isdigit() and yy.isdigit() and 0 <= int(xx) <= 9 and 0 <= int(yy) <= 9:
+            if name not in list_cows:
+                print('Cannot add unknown monster')
+            else:
+                print(f'Added monster {name} with health {hp} to ({xx}, {yy}) saying {hello_str}')
+                if (int(xx), int(yy)) in monsters:
+                    print('Replaced the old monster')
+                monsters[(int(xx), int(yy))] = [name, hp, hello_str]
+        else:
+            print('Invalid arguements')
+    elif len(cmd) >= 1 and cmd[0] in ['up', 'down', 'left', 'right', 'addmon']:
+        print('Invalid arguements')
+    else:
+        print('Invalid command')
+    cmd = sys.stdin.readline()
