4c8ea503357bfca352db6ffd1e3a42f8e38acfcf
Max Big <maxbig@MacBook-Air-Max.local>
2026-03-29 12:24:58 +0300

    copy of task1 from previous prac

---
+++ 20260323/1/client.py
@@ -0,0 +1,71 @@
+import sys
+import socket
+import shlex
+import cowsay
+from io import StringIO
+
+host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\\/o\\/o\\/'.   `(
+      ) .' . \\====/ . '. (
+       )  / <<    >> \\  (
+        '-._/``  ``\\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+    while msg := sys.stdin.buffer.readline():
+        cmd = shlex.split(msg.decode())
+        if len(cmd) == 1 and len(cmd[0]) <= 5:
+            if cmd[0] == 'up':
+                s.sendall('move 0 -1\n'.encode())
+            elif cmd[0] == 'down':
+                s.sendall('move 0 1\n'.encode())
+            elif cmd[0] == 'left':
+                s.sendall('move -1 0\n'.encode())
+            elif cmd[0] == 'right':
+                s.sendall('move 1 0\n'.encode())
+            else:
+                s.sendall('Invalid command\n'.encode())
+        elif len(cmd) == 9 and cmd[0] == 'addmon':
+            name = cmd[1]
+            hello_str = cmd[cmd.index('hello') + 1]
+            hp = cmd[cmd.index('hp') + 1]
+            xx, yy = cmd[cmd.index('coords') + 1:cmd.index('coords') + 3]
+            if xx.isdigit() and yy.isdigit() and 0 <= int(xx) <= 9 and 0 <= int(yy) <= 9:
+                s.sendall(msg)
+            else:
+                s.sendall('Invalid arguements\n'.encode())
+        elif len(cmd) == 2 and cmd[0] == 'attack':
+            s.sendall((' '.join(cmd) + ' 10\n').encode())
+        elif len(cmd) == 4 and cmd[0] == 'attack' and cmd[2] == 'with' and cmd[3] in ['sword', 'spear', 'axe']:
+            if cmd[3] == 'sword':
+                s.sendall((' '.join(cmd[:2]) + ' 10\n').encode())
+            elif cmd[3] == 'spear':
+                s.sendall((' '.join(cmd[:2]) + ' 15\n').encode())
+            elif cmd[3] == 'axe':
+                s.sendall((' '.join(cmd[:2]) + ' 20\n').encode())
+        elif len(cmd) >= 1 and cmd[0] in ['up', 'down', 'left', 'right', 'addmon', 'attack']:
+            s.sendall('Invalid arguements\n'.encode())
+        else:
+            s.sendall('Invalid command\n'.encode())
+        reply = s.recv(1024).rstrip().decode()
+        if ', cowfile' in reply:
+            repl = reply.split('\n')
+            print(repl[0])
+            print(cowsay.cowsay(repl[1].split(', cowfile=')[0], cowfile=jgsbat))
+        elif ', cow' in reply:
+            repl = reply.split('\n')
+            print(repl[0])
+            print(cowsay.cowsay(repl[1].split(', cow=')[0], cow=repl[1].split(', cow=')[1]))
+        else:
+            print(reply)
---
+++ 20260323/1/multi_user_dungeon.py
@@ -0,0 +1,172 @@
+import cowsay
+import sys
+from io import StringIO
+import shlex
+import cmd
+
+
+def encounter(x, y):
+    global jgsbat
+    if MUD.monsters[(x, y)][0] == 'jgsbat':
+        print(cowsay.cowsay(MUD.monsters[(x, y)][2], cowfile=jgsbat))
+    else:
+        print(cowsay.cowsay(MUD.monsters[(x, y)][2], cow=MUD.monsters[(x, y)][0]))
+
+
+class MUD(cmd.Cmd):
+    prompt = ''
+    x, y = 0, 0
+    monsters = {}
+    
+    def do_down(self, arg):
+        '''
+        moving down on 1 cell
+        '''
+        if arg == '':
+            MUD.y = (MUD.y + 1) % 10
+            print(f'Moved to ({MUD.x}, {MUD.y})')
+            if (MUD.x, MUD.y) in MUD.monsters:
+                encounter(MUD.x, MUD.y)
+        else:
+            print('Invalid arguements')
+    
+    def do_up(self, arg):
+        '''
+        moving up on 1 cell
+        '''
+        if arg == '':
+            MUD.y = (MUD.y + 9) % 10
+            print(f'Moved to ({MUD.x}, {MUD.y})')
+            if (MUD.x, MUD.y) in MUD.monsters:
+                encounter(MUD.x, MUD.y)
+        else:
+            print('Invalid arguements')
+    
+    def do_right(self, arg):
+        '''
+        moving right on 1 cell
+        '''
+        if arg == '':
+            MUD.x = (MUD.x + 1) % 10
+            print(f'Moved to ({MUD.x}, {MUD.y})')
+            if (MUD.x, MUD.y) in MUD.monsters:
+                encounter(MUD.x, MUD.y)
+        else:
+            print('Invalid arguements')
+    
+    def do_left(self, arg):
+        '''
+        moving left on 1 cell
+        '''
+        if arg == '':
+            MUD.x = (MUD.x + 9) % 10
+            print(f'Moved to ({MUD.x}, {MUD.y})')
+            if (MUD.x, MUD.y) in MUD.monsters:
+                encounter(MUD.x, MUD.y)
+        else:
+            print('Invalid arguements')
+    
+    def do_addmon(self, arg):
+        '''
+        adding a monster on the cell with hello phrase and hp
+        '''
+        command = shlex.split(arg)
+        if len(command) == 8:
+            name = command[0]
+            hello_str = command[command.index('hello') + 1]
+            hp = command[command.index('hp') + 1]
+            xx, yy = command[command.index('coords') + 1:command.index('coords') + 3]
+            if xx.isdigit() and yy.isdigit() and hp.isdigit() and 0 <= int(xx) <= 9 and 0 <= int(yy) <= 9:
+                if name not in MUD.list_cows:
+                    print('Cannot add unknown monster')
+                else:
+                    print(f'Added monster {name} with health {hp} to ({xx}, {yy}) saying {hello_str}')
+                    if (int(xx), int(yy)) in MUD.monsters:
+                        print('Replaced the old monster')
+                    MUD.monsters[(int(xx), int(yy))] = [name, int(hp), hello_str]
+            else:
+                print('Invalid arguements')
+        else:
+            print('Invalid arguements')
+    
+    def do_attack(self, arg):
+        '''
+        attacking a specific monster on the current cell using maybe custom weapon
+        '''
+        command = shlex.split(arg)
+        if len(command) not in [1, 3]:
+            print('Invalid arguements')
+        elif (MUD.x, MUD.y) not in MUD.monsters:
+            print(f'No {command[0]} here')
+        elif MUD.monsters[(MUD.x, MUD.y)][0] != command[0]:
+            print(f'No {command[0]} here')
+        elif len(command) == 1:
+            damage = min(10, MUD.monsters[(MUD.x, MUD.y)][1])
+            print(f'Attacked {MUD.monsters[(MUD.x, MUD.y)][0]}, damage {damage} hp')
+            MUD.monsters[(MUD.x, MUD.y)][1] -= damage
+            if MUD.monsters[(MUD.x, MUD.y)][1] == 0:
+                print(f'{MUD.monsters[(MUD.x, MUD.y)][0]} died')
+                del MUD.monsters[(MUD.x, MUD.y)]
+            else:
+                print(f'{MUD.monsters[(MUD.x, MUD.y)][0]} now has {MUD.monsters[(MUD.x, MUD.y)][1]}')
+        elif len(command) == 3 and command[1] == 'with':
+            weapon = command[2]
+            strength = 0
+            if weapon == 'sword':
+                strength = 10
+            elif weapon == 'spear':
+                strength = 15
+            elif weapon == 'axe':
+                strength = 20
+            else:
+                print('Unknown weapon')
+            if strength > 0:
+                if (MUD.x, MUD.y) in MUD.monsters:
+                    damage = min(strength, MUD.monsters[(MUD.x, MUD.y)][1])
+                    print(f'Attacked {MUD.monsters[(MUD.x, MUD.y)][0]}, damage {damage} hp')
+                    MUD.monsters[(MUD.x, MUD.y)][1] -= damage
+                    if MUD.monsters[(MUD.x, MUD.y)][1] == 0:
+                        print(f'{MUD.monsters[(MUD.x, MUD.y)][0]} died')
+                        del MUD.monsters[(MUD.x, MUD.y)]
+                    else:
+                        print(f'{MUD.monsters[(MUD.x, MUD.y)][0]} now has {MUD.monsters[(MUD.x, MUD.y)][1]}')
+                else:
+                    print('No monster here')
+        else:
+            print('Invalid arguements')
+    
+    def complete_attack(self, text, line, begidx, endidx):
+        command = shlex.split(line + '.')
+        if len(command) == 2:
+            return [mon for mon in MUD.list_cows if mon.startswith(text)]
+        if len(command) == 4 and command[2] == 'with':
+            return [weapon for weapon in ['sword', 'spear', 'axe'] if weapon.startswith(text)]
+    
+    def default(self, arg):
+        print('Invalid command')
+    
+    def do_EOF(self, arg):
+        return 1
+
+
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\\/o\\/o\\/'.   `(
+      ) .' . \\====/ . '. (
+       )  / <<    >> \\  (
+        '-._/``  ``\\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+list_cows = cowsay.list_cows()
+list_cows.append('jgsbat')
+MUD.list_cows = list_cows
+if __name__ == '__main__':
+    print("<<< Welcome to Python-MUD 0.1 >>>")
+    MUD().cmdloop()
